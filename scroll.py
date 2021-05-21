a1b36d46d94863851c6284b85d61655f5c059d2e
wlumumba <wyclifflumumba@gmail.com>
2021-05-21 10:48:13 -0500

    added script

---
+++ scroll.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+
+while True:
+    time.sleep(10)
+    """ #if you wanted mouse movement instead!
+    for i in range(1, 144, 4):
+        pyautogui.moveTo(960, i * 4)
+    """
+    pyautogui.press('shift')
+    print("Shift button pressed at {}".format(time.strftime('%I:%M:%S')))
